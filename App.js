3dd0c2b24ce1435290f2406b4c98d5f5f98dc4c5
shaba-imran <imranusmanshaba@gmail.com>
2023-06-03 18:07:03 +0100

    Use android statusbar

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { StatusBar, StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={{ flex: 1 }}>
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
         <View style={{ padding: 16, backgroundColor: 'green' }}>
           <Text>search</Text>
         </View>
         <View style={{ flex: 1, padding: 16, backgroundColor: 'blue' }}>
           <Text>list</Text>
         </View>
-        <StatusBar style="auto" />
+        <ExpoStatusBar style="auto" />
       </SafeAreaView>
     </>
   );
